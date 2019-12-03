d0e08239e42f1da2862d15ae66eb64c6bf7cf0e8
Antonio <antonio.galeazzi@txs.de>
2019-12-03 16:16:06 +0100

    added some simple tests

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,5 +1,6 @@
 module Day01
-    ( calculateTotalFuel
+    ( calculateTotalFuel,
+      modules
     ) where
 
 type Module = Int
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import Test.Hspec
+import Day01
+import Day02
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Day01.calculateTotalFuel" $ do
+      it "calculates the right amount of fuel" $ do
+        Day01.calculateTotalFuel Day01.modules `shouldBe` (4866824 :: Int)
+
+      it "handles an empty list" $ do
+        Day01.calculateTotalFuel [] `shouldBe` (0 :: Int)
